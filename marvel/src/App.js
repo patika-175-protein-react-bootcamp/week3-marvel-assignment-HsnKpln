8f04558f8914356d2be9f919dd7408e31bee05e3
hkpln <h.kpln0681@gmail.com>
2022-04-02 22:57:05 +0300

    first commit

---
+++ marvel/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Cards from './components/Cards';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Cards></Cards>
+    </div>
+  );
+}
+
+export default App;
